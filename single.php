74c742f75435e017f643bc06866ad453ebbf0093
Olkobarylko <olegbarylko81@gmail.com>
2024-03-28 23:45:11 +0200

    some fixes

--- single.php
+++ single.php
@@ -5,16 +5,22 @@ get_header();
 <main class="post">
     <div class="site-container">
         <article class="article">
+
             <h1 class="article__title">
                 <?php the_title() ?>
             </h1>
-            <div class="article__image">
-                <?php the_post_thumbnail() ?>
-            </div>
 
-            <div class="article__content">
-                <?php the_content() ?>
-            </div>
+            <?php if (has_post_thumbnail()) : ?>
+                <div class="article__image">
+                    <?php the_post_thumbnail() ?>
+                </div>
+            <?php endif ?>
+
+            <?php if (has_excerpt()) : ?>
+                <div class="article__content">
+                    <?php the_content() ?>
+                </div>
+            <?php endif ?>
 
             <div class="article__comments">
                 <?php
